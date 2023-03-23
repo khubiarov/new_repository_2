7ab762e7571c819448561cd9832d6d6bdbc931cc
Khubiarov <nrchubiarov@mail.ru>
2023-03-23 16:45:20 +0300

    edition gor task

--- str_func.py
+++ str_func.py
@@ -2,7 +2,4 @@ def str_func(source_string):
     '''Принимает строку, возвращает ее же в верхнем регитсре'''
     return source_string.upper()
 
-def first_letter(source_string):
-    '''input string , return string with first-letter in high register'''
-    numb = len(source_string)
-    return f'{source_string[0].upper}{source_string[1:numb]}'
+
